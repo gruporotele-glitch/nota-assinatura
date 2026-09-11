fc7a26951e6806682481b6d87826fb11656d6eee
gruporotele-glitch <gruporotele@gmail.com>
2026-09-11 15:08:07 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'nota-assinatura-v5';
+const CACHE = 'nota-assinatura-v7';
 const ARQUIVOS = [
-  './', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
+  './', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo.png',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf-lib/1.17.1/pdf-lib.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js'
